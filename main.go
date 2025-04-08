c7d54e11f2d21af47d039b0896014a47cb60dedf
Tanaka Toshihiro <tanak0to@ceicent.alfa.homelinux.org>
2025-04-08 14:29:56 +0900

    1st commit

---
+++ main.go
@@ -0,0 +1,26 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+)
+
+func main() {
+	// Create a new Gin router
+	router := gin.Default()
+
+	// Define a simple GET endpoint
+	router.GET("/hello/:name", func(c *gin.Context) {
+		name := c.Param("name")
+		c.String(http.StatusOK, makeGreeting(name))
+	})
+	router.Run(":8080")
+}
+
+func makeGreeting(name string) string {
+	// Create a greeting message
+	return fmt.Sprintf("Hello, %d", name)
+
+}
